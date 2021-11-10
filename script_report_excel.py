6e67734c6dacca8e13201e12ffa18307e281cd6d
Albertech <algcmusic18@gmail.com>
2021-11-10 19:58:12 -0400

    Genera el archivo excel

--- script_report_excel.py
+++ script_report_excel.py
@@ -1,7 +1,12 @@
-import pandas as pd
-import openpyxl
+from openpyxl import Workbook, load_workbook
 
-archivo_data = pd.read_csv('vulneibilidades.csv')
+#abre libro de trabajo
+wb = Workbook()
+#hoja activa
+ws = wb.active
+ws.title = "Data"
+#agrega una fila con los datos ingresados en forma de texto
+ws.append(["Asset UUID","CVE","CVSS","CVSS Base Score", "CVSS Temporal Score ","CVSS Temporal Vector ","CVSS Vector ","CVSS3 Base Score ","CVSS3 Temporal Score ","CVSS3 Temporal Vector ","CVSS3 Vector","Description" ,"FQDN" ,"Host" ,"Host End" ,"Host Start" ,"IP Address" ,"MAC Address" ,"Name" ,"NetBios","OS","Plugin Family","Plugin ID","Plugin Output ","Port ","Protocol ","Risk ","See Also","Solution","Synopsis" ,"System Type" ,"Vulnerability Priority Rating (VPR)" ,"Vulnerability State","Age" ,"First Seen","Last Seen","Recast Reason"])
 
-tabla_orden = archivo_data.pivot_table(index='IP Address', columns='Name', values='OS', aggfunc='order')
-print(tabla_orden)
+#guarda el excel con los cambios realizados
+wb.save('reportVulnerability.xlsx')
